f699d236f85917407b1e367a0373d821916d7e57
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-27 14:01:50 -0300

    feat: adiciona apexcharts para visualização de respostas

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,6 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
 window.FilePond = FilePond;
 window.FilePondPluginImagePreview = FilePondPluginImagePreview;
+
+import ApexCharts from 'apexcharts'
+window.ApexCharts = ApexCharts;
