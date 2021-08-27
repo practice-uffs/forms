9bd6e1ce5b90f5450aefe0c7022d351ff7fd0dad
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-27 16:38:52 -0300

    feat: ajusta endpoints de api

---
+++ app/Http/Controllers/API/FormController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Model\Form;
+use Orion\Http\Controllers\Controller;
+
+class FormController extends Controller
+{
+    protected $model = Form::class;
+}
---
+++ app/Http/Controllers/API/ReplyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Model\Reply;
+use Orion\Http\Controllers\Controller;
+
+class ReplyController extends Controller
+{
+    protected $model = Reply::class;
+}
