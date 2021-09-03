08bc39d48fba01fa9d52361fbdfa5d8d15cd4a60
Fernando Bevilacqua <dovyski@gmail.com>
2021-09-03 09:30:06 -0300

    feat: adiciona código QR real para responder enquete

--- resources/views/form/edit.blade.php
+++ resources/views/form/edit.blade.php
@@ -2,18 +2,29 @@
 @section('content')
 
 <section>
-    <div class="container">
+    <div x-data="{enlarge: false}" class="container">
         <header class="section-header">
         </header>
+
+        <div class="absolute top-0 left-0 z-20 w-screen h-screen bg-white pointer-events-none flex items-center justify-center" x-show="enlarge">
+            <div class="m-auto">
+                {!! QrCode::size(512)->generate($reply_link); !!}
+            </div>
+        </div>
 
         <div class="row mb-8">
             <div class="row">
                 <div class="col-4"></div>
                 <div class="col-8 content-end text-right">
-                    <img src="{{ asset('img/qrcode.svg') }}" class="w-24 float-right ml-4" title="">
-                    <p class="font-semibold text-lg">Acesso para responder</p>
-                    <p class="text-md text-gray-500 mt-1">Para que outras pessoas possam responder, compartilhe o código QR ao lado ou o link baixo:</p>
-                    <a href="{{ $reply_link }}" class="text-xl font-semibold text-blue-400" target="_blank">{{ $reply_link }}</a>
+                    <div class="float-right ml-6 enlarge-img-hover" x-on:mouseover="enlarge = true" x-on:mouseleave="enlarge = false" >
+                        {!! QrCode::size(100)->generate($reply_link); !!}
+                    </div>
+                    
+                    <div class="z-30">
+                        <p class="font-semibold text-lg">Acesso para responder</p>
+                        <p class="text-md text-gray-500 mt-1">Para que outras pessoas possam responder, compartilhe o código QR ao lado ou o link baixo:</p>
+                        <a href="{{ $reply_link }}" class="text-xl font-semibold text-blue-400" target="_blank">{{ $reply_link }}</a>
+                    </div>
                 </div>
             </div>
         </div>
