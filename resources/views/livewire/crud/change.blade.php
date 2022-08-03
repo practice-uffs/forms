cc5a1a240ea37cdbc78425c7930919647788fdfd
Adriano <zorteaadriano@gmail.com>
2022-08-03 01:36:16 -0300

    ajustando exibição de perguntas, separação por blocos estilo card personalizados, alinhamentos e cores para cada tipo de pergunta

--- resources/views/livewire/crud/change.blade.php
+++ resources/views/livewire/crud/change.blade.php
@@ -14,142 +14,155 @@
         @enderror
 
         <div class="mb-4 pb-4">
+            @php $counter_question = 1; @endphp 
             @foreach ($fields as $key => $field)
                 @if (isset($field['show']) && ( ($editing && !Str::contains($field['show'], 'edit')) || (!$editing && !Str::contains($field['show'], 'create'))))
                     @continue
                 @endif
-
+                <div class="container-question mb-5 p-4 pb-5 shadow-sm">
                 @switch(@$field['type'])
-                @case('boolean')
-                    <label class="cursor-pointer label flex">
-                        <span class="label-text bold">{{ $field['label'] }} <br /><span class="text-gray-400">{{ @$field['placeholder'] }}</span></span> 
-                        <div>
-                            <input wire:model="{{ $key }}" type="checkbox"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="toggle toggle-primary"> 
-                            <span class="toggle-mark"></span>
-                        </div>
-                    </label>
-                    <br><br>
-                    @break
-                @case('select')
-                    <label class="label">
-                        <span class="label-text"><strong>{{ $field['label'] }}</strong></span> 
-                        <a href="#" class="label-text-alt"></a>
-                    </label> 
-                    <select wire:model="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="select select-bordered w-full @error($key) select-error @enderror">
-                        <option value=""> -- selecione --</option> 
-                        @foreach ($field['options'] as $info)
-                            <option value="{{ $info['id']}}">{{ $info['text']}}</option> 
-                        @endforeach
-                    </select> 
-                    @break
-                @case('radio')
-                    <span class="label-text"><strong>{{ $field['label'] }}</strong></span> 
-                    <div class="flex flex-row mt-2">
-                        @foreach ($field['options'] as $info)
-                            <label class="cursor-pointer mr-5">
-                                <div class="inline-block">
-                                    <input wire:model="{{ $key }}" type="radio" checked="checked"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="radio radio-primary" value="{{ $info['id']}}"> 
-                                    <span class="radio-mark mr-1"></span>
-                                </div>
-                                <span class="label-text {{ @$field['style'] }}">{{ $info['text']}}</span> 
-                            </label>
-                        @endforeach
-                    </div>
-                    @break
 
-                @case('checkbox')
-                    <span class="label-text"><strong>{{ $field['label'] }}</strong></span> 
-                    <div class="flex flex-row mt-2">
-                        @foreach ($field['options'] as $checkbox_index => $info)
-                            <label class="cursor-pointer mr-5">
-                                <div class="inline-block">
-                                    <input wire:model="{{ $key }}#{{ $checkbox_index }}"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.checked, '{{ $field['type'] }}')" type="checkbox" checked="checked" class="checkbox checkbox-primary" value="{{ $info['id']}}"> 
-                                    <span class="radio-mark mr-1"></span>
-                                </div>
-                                <span class="label-text {{ @$field['style'] }}">{{ $info['text']}}</span> 
-                            </label>
-                        @endforeach
-                    </div>
-                    @break
-                @case('date')
-                    <label for="{{ $key }}" class="label">
-                        <span class="label-text"><strong>{{ $field['label'] }}</strong></span>
-                    </label>
-                    <input wire:model="{{ $key }}" type="date" name="{{ $key }}"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" placeholder="{{ @$field['placeholder'] }}" class="input input-bordered @error($key) input-error @enderror max-w-md" />
-                    @break
-                @case('file')
-                    <label class="label">
-                        <span class="label-text"><strong>{{ $field['label'] }}</strong></span> 
-                        <a href="#" class="label-text-alt"></a>
-                    </label>
-                    <div
-                        class="float-left w-100"
-                        wire:ignore
-                        x-data="{pond: null}"
-                        x-init="
-                            pond = FilePond.create($refs.input);
-                            pond.setOptions({
-                                allowMultiple: true,
-                                labelIdle:'{{ @$field['placeholder'] }}',
-                                labelFileProcessingComplete: 'Upload finalizado',
-                                server: {
-                                    process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-                                        @this.upload('{{ $key }}', file, load, error, progress);
-                                        ProgressBar.fieldChanged('{{ $key }}', 1, '{{ $field['type'] }}');
-                                    },
-                                    revert: (filename, load) => {
-                                        @this.removeUpload('{{ $key }}', filename, load);
-                                        ProgressBar.fieldChanged('{{ $key }}',  -1, '{{ $field['type'] }}');
-                                    },
-                                },
-                            });
-                        ">
-                        <input type="file" name="{{ $key }}" x-ref="input">
-                    </div>
-                    @break
-                @case('poll')
-                    <div class="flex w-full">
-                        <div class="w-1/2 p-2">
-                            <label for="{{ $key }}" class="label">
-                                <span class="label-text"><strong>{{ $field['label'] }}</strong></span>
-                            </label>
-                            <textarea wire:model="{{ $key }}" name="{{ $key }}" placeholder="{{ @$field['placeholder'] }}" class="textarea textarea-bordered h-screen w-full @error($key) textarea-error @enderror"></textarea>
+                    @case('boolean')
+                        <label class="cursor-pointer label flex">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }} <br/><span class="text-gray-400">{{ @$field['placeholder'] }}</span></span> 
+                            <div>
+                                <input wire:model="{{ $key }}" type="checkbox"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="toggle toggle-primary"> 
+                                <span class="toggle-mark"></span>
+                            </div>
+                        </label>
+                        @break
+
+                    @case('select')
+                        <label class="label">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span>
+                        </label> 
+                        <select wire:model="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="select select-bordered w-full @error($key) select-error @enderror">
+                            <option value=""> -- selecione --</option> 
+                            @foreach ($field['options'] as $info)
+                                <option value="{{ $info['id']}}">{{ $info['text']}}</option> 
+                            @endforeach
+                        </select> 
+                        @break
+
+                    @case('radio')
+                        <label class="label">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span> 
+                        </label>
+                        <div class="mt-2">
+                            @foreach ($field['options'] as $info)
+                                <label class="cursor-pointer mr-5">
+                                    <div class="inline-block">
+                                        <input wire:model="{{ $key }}" type="radio" checked="checked"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" class="radio radio-primary" value="{{ $info['id']}}"> 
+                                        <span class="radio-mark mr-1"></span>
+                                    </div>
+                                    <span class="h4 {{ @$field['style'] }}">{{ $info['text']}}</span> 
+                                </label>
+                            @endforeach
                         </div>
-                        <div class="w-1/2 p-2">
-                            <label class="label">
-                                <span class="label-text"><strong>Visualização</span>
-                            </lab</strong>el>
-                            <div class="mockup-window bg-base-300">
-                                <div class="absolute top-3 right-2">kjh</div>
-                                <div x-data="{ content: null }" x-init="{ content: @entangle('data.poll') }" class="w-full h-screen p-2 bg-gray-100 overflow-scroll">
-                                    <div x-on:blur="content = $event.target.innerHTML">
-                                        {!! $poll_view !!}
+                        @break
+
+                    @case('checkbox')
+                        <label class="label">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span> 
+                        </label>
+                        <div class="mt-2">
+                            @foreach ($field['options'] as $checkbox_index => $info)
+                                <label class="cursor-pointer mr-5">
+                                    <div class="inline-block">
+                                        <input wire:model="{{ $key }}#{{ $checkbox_index }}"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.checked, '{{ $field['type'] }}')" type="checkbox" checked="checked" class="checkbox checkbox-primary" value="{{ $info['id']}}"> 
+                                        <span class="radio-mark mr-1"></span>
+                                    </div>
+                                    <span class="h4 {{ @$field['style'] }}">{{ $info['text']}}</span> 
+                                </label>
+                            @endforeach
+                        </div>
+                        @break
+
+                    @case('date')
+                        <label class="label" for="{{ $key }}">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span>
+                        </label>
+                        <input wire:model="{{ $key }}" type="date" name="{{ $key }}"  onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" placeholder="{{ @$field['placeholder'] }}" class="input input-bordered @error($key) input-error @enderror max-w-md" />
+                        @break
+                    @case('file')
+                        <label class="label">
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span> 
+                            <a href="#" class="h4-alt"></a>
+                        </label>
+                        <div
+                            class="float-left w-100"
+                            wire:ignore
+                            x-data="{pond: null}"
+                            x-init="
+                                pond = FilePond.create($refs.input);
+                                pond.setOptions({
+                                    allowMultiple: true,
+                                    labelIdle:'{{ @$field['placeholder'] }}',
+                                    labelFileProcessingComplete: 'Upload finalizado',
+                                    server: {
+                                        process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+                                            @this.upload('{{ $key }}', file, load, error, progress);
+                                            ProgressBar.fieldChanged('{{ $key }}', 1, '{{ $field['type'] }}');
+                                        },
+                                        revert: (filename, load) => {
+                                            @this.removeUpload('{{ $key }}', filename, load);
+                                            ProgressBar.fieldChanged('{{ $key }}',  -1, '{{ $field['type'] }}');
+                                        },
+                                    },
+                                });
+                            ">
+                            <input type="file" name="{{ $key }}" x-ref="input">
+                        </div>
+                        @break
+
+                    @case('poll')
+                        <div class="flex w-full">
+                            <div class="w-1/2 p-2">
+                                <label class="label" for="{{ $key }}">
+                                    <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span>
+                                </label>
+                                <textarea wire:model="{{ $key }}" name="{{ $key }}" placeholder="{{ @$field['placeholder'] }}" class="textarea textarea-bordered h-screen w-full @error($key) textarea-error @enderror"></textarea>
+                            </div>
+                            <div class="w-1/2 p-2">
+                                <label class="label">
+                                    <span class="h4">Visualização</span>
+                                </label>
+                                <div class="mockup-window bg-base-300">
+                                    <div class="absolute top-3 right-2">kjh</div>
+                                    <div x-data="{ content: null }" x-init="{ content: @entangle('data.poll') }" class="w-full h-screen p-2 bg-gray-100 overflow-scroll">
+                                        <div x-on:blur="content = $event.target.innerHTML">
+                                            {!! $poll_view !!}
+                                        </div>
                                     </div>
                                 </div>
+                                
                             </div>
-                            
                         </div>
-                    </div>
-                    @break
-                @case('textarea')
-                    <label for="{{ $key }}" class="label">
-                        <span class="label-text"><strong>{{ $field['label'] }}</strong></span>
-                    </label>
-                    <textarea wire:model="{{ $key }}" name="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" placeholder="{{ @$field['placeholder'] }}" class="textarea textarea-bordered h-48 mb-2 @error($key) textarea-error @enderror"></textarea>
-                    @break
-                @default
-                    <label for="{{ $key }}" class="label">
-                        <span class="label-text"><strong>{{ $field['label'] }}</strong></span>
-                    </label>
-                    <input wire:model="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" type="text" name="{{ $key }}" placeholder="{{ @$field['placeholder'] }}" class="input input-bordered @error($key) input-error @enderror w-full" />
-                    @break
+                        @break
+
+                    @case('textarea')
+                        <label class="label" for="{{ $key }}" >
+                            <span class="h4">{{ $counter_question++ . '. ' . $field['label'] }}</span>
+                        </label>
+                        <textarea wire:model="{{ $key }}" name="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" placeholder="{{ @$field['placeholder'] }}" class="textarea textarea-bordered h-48 mb-2 @error($key) textarea-error @enderror"></textarea>
+                        @break
+
+                    @default
+                        <label class="label" for="{{ $key }}">
+                            <span class="h4 h3">{{ $counter_question++ . '. ' . $field['label'] }}</span>
+                        </label>
+                        <input wire:model="{{ $key }}" onChange="ProgressBar.fieldChanged('{{ $key }}', this.value, '{{ $field['type'] }}')" type="text" name="{{ $key }}" placeholder="{{ @$field['placeholder'] }}" class="input input-bordered @error($key) input-error @enderror w-full" />
+                        @break
+
                 @endswitch
 
                 @error($key)
-                    <span class="label-text-alt text-red-500">{{ $message }}</span>
+                    <div class="w-100">
+                        <span class="h4-alt text-red-500">{{ $message }}</span>
+                    </div>
                 @enderror
 
-                <br><br>
+                </div>
             @endforeach
         </div>
 
@@ -160,7 +173,7 @@
                     <button wire:click="cancel()" class="btn float-right mr-6">Cancelar</button>
                 @endif
             @else
-                <button wire:click="store({{$form->id}})" class="btn btn-wide btn-primary ml-auto mr-auto d-flex">Enviar</button>
+                <button wire:click="store({{$form->id}})" class="btn btn-wide btn-primary ml-auto mr-auto d-flex mt-20">Enviar</button>
             @endif
         @endif
     @endif
